5c097b2e73d553a304f2a5eb73de0f17afb3faf7
Divyadharshini-R-S <divyapavi008@gmail.com>
2026-07-04 23:34:43 +0530

    refactor: project structure

--- homehub-backend/src/main/java/com/divs/homehub/HomehubBackendApplication.java
+++ homehub-backend/src/main/java/com/divs/homehub/HomehubBackendApplication.java
@@ -1,8 +1,8 @@
-package com.divs.homehub.HomehubBackendApplication;
+package com.divs.homehub;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class HomehubBackendApplication {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
